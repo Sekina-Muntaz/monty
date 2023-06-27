fac59cb1d87ff7e8e31f6023d4b235d551704678
FREDRICK OKARA <okaraogunde@gmail.com>
2023-06-27 10:59:00 +0000

    updates

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
 	script_fd = fopen(argv[1], "r");
 	if (script_fd == NULL)
 		return (f_open_error(argv[1]));
-	exit_code = run_monty(script_fd);
+	exit_code = execute(script_fd);
 	fclose(script_fd);
 	return (exit_code);
 }
