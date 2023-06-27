3258a0b33d8e004c709c9ee775750aa9c7235674
FREDRICK OKARA <okaraogunde@gmail.com>
2023-06-27 11:02:43 +0000

    updates

--- _funct_3.c
+++ _funct_3.c
@@ -5,7 +5,7 @@ void _pchar(stack_t **stack, unsigned int line_number);
 void _pstr(stack_t **stack, unsigned int line_number);
 
 /**
- * monty_nop - Does absolutely nothing for the Monty opcode 'nop'.
+ * _nop - Does absolutely nothing for the Monty opcode 'nop'.
  * @stack: A pointer to the top mode node of a stack_t linked list.
  * @line_number: The current working line number of a Monty bytecodes file.
  */
@@ -16,7 +16,7 @@ void _nop(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * monty_pchar - Prints the character in the top value
+ * _pchar - Prints the character in the top value
  *               node of a stack_t linked list.
  * @stack: A pointer to the top mode node of a stack_t linked list.
  * @line_number: The current working line number of a Monty bytecodes file.
